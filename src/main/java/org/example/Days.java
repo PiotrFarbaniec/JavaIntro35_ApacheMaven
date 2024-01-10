ad9805953e6793c1afe75d9c63441f1c060c34bd
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-01-10 13:14:39 +0100

    method checking a day of the week with 93% test coverage

---
+++ src/main/java/org/example/Days.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Days {
+    public static void main(String[] args) {
+
+        System.out.println(checkingWhatDayOfTheWeekIs(7));
+    }
+
+    static String checkingWhatDayOfTheWeekIs (int dayNumber) {
+        String day;
+
+        switch (dayNumber) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+            case 7:
+                day = "Weekend";
+                break;
+            default:
+                day = "There is no such a day!";
+        }
+        return day;
+    }
+}
---
+++ src/test/java/org/example/DaysTest.java
@@ -0,0 +1,119 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaysTest {
+
+    @Test
+    void whenGivenNumberIsOneShouldReturnMonday() {
+        // given
+        int dayNumber = 1;
+        String expResult = "Monday";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsTwoShouldReturnTuesday() {
+        // given
+        int dayNumber = 2;
+        String expResult = "Tuesday";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsThreeShouldReturnWednesday() {
+        // given
+        int dayNumber = 3;
+        String expResult = "Wednesday";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsFourShouldReturnThursday() {
+        // given
+        int dayNumber = 4;
+        String expResult = "Thursday";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsFiveShouldReturnFriday() {
+        // given
+        int dayNumber = 5;
+        String expResult = "Friday";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsSixShouldReturnWeekend() {
+        // given
+        int dayNumber = 6;
+        String expResult = "Weekend";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void whenGivenNumberIsSevenShouldReturnWeekend() {
+        // given
+        int dayNumber = 7;
+        String expResult = "Weekend";
+
+        // when
+        String result = Days.checkingWhatDayOfTheWeekIs(dayNumber);
+
+        //then
+        Assertions.assertEquals(expResult, result);
+    }
+
+    @Test
+    void inOtherCasesShouldReturnExpectedMessage() {
+        // given
+        int dayNumber1 = -1;
+        int dayNumber2 = 0;
+        int dayNumber3 = 8;
+        String expResult = "There is no such a day!";
+
+        // when
+        String result1 = Days.checkingWhatDayOfTheWeekIs(dayNumber1);
+        String result2 = Days.checkingWhatDayOfTheWeekIs(dayNumber2);
+        String result3 = Days.checkingWhatDayOfTheWeekIs(dayNumber3);
+
+        //then
+        Assertions.assertEquals(expResult, result1);
+        Assertions.assertEquals(expResult, result2);
+        Assertions.assertEquals(expResult, result3);
+    }
+}
